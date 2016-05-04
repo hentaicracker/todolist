0f8d088bfaed32d1407b832b2829267cdcb5640e
hentaicracker <641232582@qq.com>
2016-05-04 20:32:22 +0800

    add vue config

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -15,10 +15,30 @@ module.exports = {
 
   module: {
     loaders: [
+      { test: /\.vue$/, loader: 'vue'},
+      { test: /\.js$/, loader: 'babel', exclude: /node_modules/},
       { test: /\.scss$/, loader: 'style!css!sass'},
       { test: /\.(svg|ttf|eot|woff|woff2)(\?.*$|$)/, loader: 'url-loader?importLoaders=1&limit=1000&name=/fonts/[name].[ext]'},
       { test: /\.(png|jpg|gif)$/, loader: 'url-loader'}
     ]
+  },
+
+  vue: {
+    loaders: {
+      css: 'style!css!autoprefixer!less'
+    }
+  },
+
+  babel: {
+    presets: ['es2015'],
+    plugins: ['transform-runtime']
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.vue'],
+    alias: {
+      components: path.join(__dirname, './src/components')
+    }
   },
 
   devServer: {
