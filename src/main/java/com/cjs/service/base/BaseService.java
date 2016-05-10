9f8d0bb26ab069713945466c62843859dde64c52
zqw <249112805@qq.com>
2016-05-10 19:41:14 +0800

    删除多余代码

--- src/main/java/com/cjs/service/base/BaseService.java
+++ src/main/java/com/cjs/service/base/BaseService.java
@@ -14,22 +14,10 @@ public interface BaseService<T> {
     public Serializable save(T t);
 
     /**
-     * <p>保存对象集合</p>
-     * @param ts 需要保存的对象集合
-     */
-    public void saveAll(List<T> ts);
-
-    /**
      * <p>更新一个对象</p>
      * @param t 需要更新的对象
      */
     public void update(T t);
-
-    /**
-     * <p>更新对象集合</p>
-     * @param ts 需要更新的对象集合
-     */
-    public void updateAll(List<T> ts);
 
     /**
      * <p>保存或更新一个对象</p>
@@ -38,24 +26,11 @@ public interface BaseService<T> {
     public void saveOrUpdate(T t);
 
     /**
-     * <p>保存或更新对象集合</p>
-     * @param ts 需要保存或更新的对象集合
-     */
-    public void saveOrUpdateAll(List<T> ts);
-
-    /**
      * <p>删除对象</p>
      * 此处根据传入对象的主键，删除数据库中对应的数据
      * @param t 需要删除的数据（包含主键，即id）
      */
     public void delete(T t);
-
-    /**
-     * <p>删除对象集合</p>
-     * 此处根据传入对象的主键集合，删除数据库中对应的数据
-     * @param ts 需要删除的数据集合（包含主键集合，即ids）
-     */
-    public void deleteAll(List<T> ts);
 
     /**
      * <p>根据ID获取对象</p>
@@ -69,38 +44,12 @@ public interface BaseService<T> {
     public List<T> findAll();
 
     /**
-     * <p>查询对应表所有数据的总数</p>
-     * @param hql hql语句
-     * @param values 属性值的结合
-     * @return int 表中记录总数
-     */
-    public int findAllCount(String hql, Serializable... values);
-
-    /**
-     * <p>查询对应表所有数据的总数</p>
-     * @param hql hql语句
-     * @param values hql中需要注入的参数
-     * @return int 表中记录总数
-     */
-    public int countByHql(String hql, Serializable... values);
-
-    /**
      * <p>根据hql语句和对应的属性值查询</p>
      * @param hql hql语句
      * @param objects hql中需要注入的参数
      * @return List<T> 符合条件的对象列表
      */
     public List<T> findByHql(String hql, Object... objects);
-
-    /**
-     * <p>通过编写hql查询,来进行分页（分页）</p>
-     * @param hql hql语句
-     * @param start 起始位置
-     * @param limit 总数
-     * @param objects hql中需要注入的参数
-     * @return List<T> 符合条件的对象列表
-     */
-    public List<T> findByHqlPage(String hql, int start, int limit, Object... objects);
 
     /**
      * <p>通过编写hql查询唯一对象</p>
@@ -126,41 +75,12 @@ public interface BaseService<T> {
     public List<T> findByProperty(String propertyName, Serializable value);
 
     /**
-     * <p>通过一个属性和对应值去相关表中查询数据（分页）</p>
-     * @param propertyName  属性名
-     * @param value 对应属性值
-     * @param start 起始位置
-     * @param limit 总数
-     * @return List<T> 符合条件的对象列表
-     */
-    public List<T> findByProperty(String propertyName, Serializable value, long start, long limit);
-
-    /**
-     * <p>通过一个属性和对应值去相关表中查询数据(分页)</p>
-     *
-     * @param propertyName  属性名
-     * @param value 对应属性值
-     * @param start 起始位置
-     * @param limit 总数
-     * @return List<T> 符合条件的对象列表
-     */
-    public List<T> findByPropertyPage(String propertyName, Serializable value, int start, int limit);
-
-    /**
      * <p>通过属性名和属性值查询唯一对象</p>
      * @param propertyName 属性名
      * @param value 属性值
      * @return T 符合条件的对象
      */
     public T findByPropertyUnique(String propertyName, Serializable value);
-
-    /**
-     * <p>查询所有数据（分页）</p>
-     * @param start 起始位置
-     * @param limit 总数
-     * @return List<T> 符合条件的对象列表
-     */
-    public List<T> findAll(int start, int limit);
 
     /**
      * <p>通过属性名与属性值删除数据</p>
@@ -171,12 +91,4 @@ public interface BaseService<T> {
      */
     public int deleteByProperty(String propertyName, Serializable value);
 
-    /**
-     * <p>通过多个属性名与属性值删除数据</p>
-     *
-     * @param propertyNames 属性名数组
-     * @param values        属性值数组
-     * @return
-     */
-    public int deleteByProperties(String[] propertyNames, Object[] values);
 }
--- src/main/java/com/cjs/service/base/BaseServiceImpl.java
+++ src/main/java/com/cjs/service/base/BaseServiceImpl.java
@@ -21,18 +21,8 @@ public class BaseServiceImpl<T extends Serializable> implements BaseService<T> {
     }
 
     @Override
-    public void saveAll(List<T> ts) {
-        baseDao.saveAll(ts);
-    }
-
-    @Override
     public void update(T t) {
         baseDao.update(t);
-    }
-
-    @Override
-    public void updateAll(List<T> ts) {
-        baseDao.updateAll(ts);
     }
 
     @Override
@@ -41,18 +31,8 @@ public class BaseServiceImpl<T extends Serializable> implements BaseService<T> {
     }
 
     @Override
-    public void saveOrUpdateAll(List<T> ts) {
-        baseDao.saveOrUpdateAll(ts);
-    }
-
-    @Override
     public void delete(T t) {
         baseDao.delete(t);
-    }
-
-    @Override
-    public void deleteAll(List<T> ts) {
-        baseDao.deleteAll(ts);
     }
 
     @Override
@@ -66,23 +46,8 @@ public class BaseServiceImpl<T extends Serializable> implements BaseService<T> {
     }
 
     @Override
-    public int findAllCount(String hql, Serializable... values) {
-        return baseDao.findAllCount(hql, values);
-    }
-
-    @Override
-    public int countByHql(String hql, Serializable... values) {
-        return baseDao.countByHql(hql, values);
-    }
-
-    @Override
     public List<T> findByHql(String hql, Object... objects) {
         return baseDao.findByHql(hql, objects);
-    }
-
-    @Override
-    public List<T> findByHqlPage(String hql, int start, int limit,Object... objects) {
-        return baseDao.findByHqlPage(hql, start, limit, objects);
     }
 
     @Override
@@ -101,32 +66,12 @@ public class BaseServiceImpl<T extends Serializable> implements BaseService<T> {
     }
 
     @Override
-    public List<T> findByProperty(String propertyName, Serializable value, long start, long limit) {
-        return baseDao.findByProperty(propertyName, value, start, limit);
-    }
-
-    @Override
-    public List<T> findByPropertyPage(String propertyName, Serializable value, int start, int limit) {
-        return baseDao.findByPropertyPage(propertyName, value, start, limit);
-    }
-
-    @Override
     public T findByPropertyUnique(String propertyName, Serializable value) {
         return baseDao.findByPropertyUnique(propertyName, value);
     }
 
     @Override
-    public List<T> findAll(int start, int limit) {
-        return baseDao.findAll(start, limit);
-    }
-
-    @Override
     public int deleteByProperty(String propertyName, Serializable value) {
         return baseDao.deleteByProperty(propertyName, value);
-    }
-
-    @Override
-    public int deleteByProperties(String[] propertyNames, Object[] values) {
-        return baseDao.deleteByProperties(propertyNames, values);
     }
 }
