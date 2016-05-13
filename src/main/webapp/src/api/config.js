7d92e98a8e187d71f7e0a67afdea791f76e9c2ec
hentaicracker <641232582@qq.com>
2016-05-13 20:20:36 +0800

    build

--- src/main/webapp/src/api/config.js
+++ src/main/webapp/src/api/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  loginUrl: 'todolist/user/userLogin',
-  userDataUrl: 'todolist/user/findCurrentUser',
-  tasksUrl: 'todolist/user/findUserOwnTask',
+  loginUrl: '/todolist/user/userLogin',
+  userDataUrl: '/todolist/user/findCurrentUser',
+  tasksUrl: '/todolist/user/findUserOwnTask',
   saveTaskUrl: '',
   saveContentUrl: '',
   toggleTaskUrl: '',
