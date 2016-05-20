cfdacb6918d711364aac1e3f3d5d6ae32b498b71
hentaicracker <641232582@qq.com>
2016-05-20 22:55:42 +0800

    map style changed

--- src/main/webapp/webpack.config.js
+++ src/main/webapp/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, './build'),
     filename: '[name].bundle.js',
-    publicPath: '/build/'
+    publicPath: '/todolist/build/'
   },
 
   module: {
